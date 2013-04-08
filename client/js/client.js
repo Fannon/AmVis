5115655fbc4d1023685b2aad8b6343576023301d
Fannon <heimlersimon@gail.com>
2013-04-08 15:00:58 +0200

    Kommentare, Ideen

---
+++ client/js/client.js
@@ -0,0 +1,19 @@
+/////////////////////////////////////////
+// Client Main JavaScript ///////////////
+/////////////////////////////////////////
+
+
+// Starting Camera
+
+
+//// read Camerainformations
+
+//// process Camerainformations
+
+//// create Colorpalette
+var colorPalette = [];
+
+
+// Start / Change Module
+
+// Read Options, execute Options
