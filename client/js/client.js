0318c61b22976cc14de109767985bb1cb1959073
Fannon <heimlersimon@gail.com>
2013-04-08 15:45:02 +0200

    Umstrukturierung: Controller hat jetzt eigenen Unterordner

--- client/js/client.js
+++ client/js/client.js
@@ -17,3 +17,24 @@ var colorPalette = [];
 // Start / Change Module
 
 // Read Options, execute Options
+
+
+
+
+// Sandbox:
+// http://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm
+var video = document.querySelector("#videoElement");
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+
+function videoError(e) {
+    // do something
+}
