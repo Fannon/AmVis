550b2df596afcf5660e3185c0aa2592584f50cfb
Blacklist <blacklist.mod@web.de>
2013-05-05 23:36:49 +0200

    tested three.js-Shader

--- client/js/visualisation.js
+++ client/js/visualisation.js
@@ -143,6 +143,88 @@ amvis.vis.programs.simpleBackground.init = function() {
     console.log('simpleBackground.init();');
     var self = amvis.vis.programs.simpleBackground;
 
+    // set the scene size
+    var WIDTH = 400,
+        HEIGHT = 300;
+
+    // set some camera attributes
+    var VIEW_ANGLE = 45,
+        ASPECT = WIDTH / HEIGHT,
+        NEAR = 0.1,
+        FAR = 10000;
+
+    // get the DOM element to attach to
+    // - assume we've got jQuery to hand
+    var $container = $('#container');
+
+    // create a WebGL renderer, camera
+    // and a scene
+    var renderer = new THREE.WebGLRenderer();
+    var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
+                                    ASPECT,
+                                    NEAR,
+                                    FAR  );
+    var scene = new THREE.Scene();
+
+    // the camera starts at 0,0,0 so pull it back
+    camera.position.z = 300;
+
+    // start the renderer
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // attach the render-supplied DOM element
+    $container.append(renderer.domElement);
+
+    // create the sphere's material
+    var sphereMaterial = new THREE.MeshLambertMaterial(
+    {
+        color: 0xCC0000
+    });
+
+    // set up the sphere vars
+    var radius = 50, segments = 16, rings = 16;
+
+    // create a new mesh with sphere geometry -
+    // we will cover the sphereMaterial next!
+    var sphere = new THREE.Mesh(
+       new THREE.SphereGeometry(radius, segments, rings),
+       sphereMaterial);
+
+    // add the sphere to the scene
+    scene.add(sphere);
+
+    // and the camera
+    scene.add(camera);
+
+    // create a point light
+    var pointLight = new THREE.PointLight( 0xFFFFFF );
+
+    // set its position
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+
+    // add to the scene
+    scene.add(pointLight);
+
+    // draw!
+    renderer.render(scene, camera);
+
+    /**
+     * Assume we have jQuery to hand
+     * and pull out from the DOM the
+     * two snippets of text for
+     * each of our shaders
+     */
+    var vShader = $('#vertexshader');
+    console.log(vShader.text());
+    var fShader = $('#fragmentshader');
+    var shaderMaterial =
+      new THREE.ShaderMaterial({
+        vertexShader:   vShader.text(),
+        fragmentShader: fShader.text()
+      });
+
 };
 
 amvis.vis.programs.simpleBackground.animate = function() {
